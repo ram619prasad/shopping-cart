a3b6a1549868d0407c7c0467282945616ae2e4ad
RamPrasadReddy Maddi <ram.prasad@introhive.com>
2017-07-22 11:23:20 +0530

    ShoppingCart: FontAwesome and Sample thumbnails.
    * Included font-awesome to use the icon pack.
    
    * Sample thumbnail template has been added.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var exphbs  = require('express-handlebars');
 
 var index = require('./routes/index');
 
@@ -11,7 +12,9 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('.hbs', exphbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+exphbs.create().getPartials();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Ramp Store' });
+  res.render('shop/index', { title: 'Ramp Store' });
 });
 
 module.exports = router;
